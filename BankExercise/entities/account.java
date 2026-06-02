47c89f87b2a1700043e1d434dee1402bec329b0d
Dev-Lu <luizgustav796@gmail.com>
2026-06-02 20:02:44 -0300

    correção 01

--- BankExercise/entities/account.java
+++ BankExercise/entities/account.java
@@ -1,15 +1,41 @@
 package BankExercise.entities;
 
 public class account {
-    private String titular;
-    private  int holder;
+    private String  holder;
+    private int number;
     private double balance = 0;
-    public account() {}
-    public account(String titular, double balance, int holder ) {
-        this.titular = titular;
-        this.balance = balance;
+
+    public account() {
+    }
+
+    public account(String holder, double initialDeposit, int number) {
+        this.number= number;
+        this.holder = holder;
+        deposit(initialDeposit);
+
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getHolder() {
+        return holder;
+    }
+
+    public void setHolder(String holder) {
         this.holder = holder;
     }
-    public double depositMoney void() {
-        System.out.println("hello");
-    } }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void deposit(double amount){
+        balance += amount;
+    }
+
+    public void withdraw( double amount){
+        balance -= amount + 5.00;
+    }
+}
