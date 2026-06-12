0a10effe02556bffe52211c38d2afb976c0f9031
Dev-Lu <luizgustav796@gmail.com>
2026-06-11 23:16:53 -0300

    .
    .

---
+++ vetor/main.java
@@ -0,0 +1,21 @@
+package vetor;
+import java.util.Locale;
+import java.util.Scanner;
+public class main {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner scan = new Scanner(System.in);
+        System.out.println("enter eith the notes");
+        int n = scan.nextInt();
+        double[] vect = new double[n];
+        for (int i=0; i<n; i++) {
+            vect[i] = scan.nextDouble();
+        }
+        double sum = 0.0;
+        for (int i=0; i<n; i++) {
+            sum += vect[i];
+        }
+        double avg = sum / n;
+        System.out.printf("AVERAGE HEIGHT: %.2f%n", avg);
+        scan.close();
+    }}
