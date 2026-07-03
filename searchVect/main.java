5e3a7b849b45e7a5e7bf124e947628b7455a3a82
Dev-Lu <luizgustav796@gmail.com>
2026-07-02 21:56:08 -0300

    .
    .

--- searchVect/main.java
+++ searchVect/main.java
@@ -17,22 +17,33 @@ public class main {
             for (int i= 1;i < nomes.length;i++){
             System.out.println(nomes[i]+ " "+ i);
             }}
-        System.out.println("deseja alterar algum valor? y/n");
+        System.out.println("deseja alterar algum nome? y/n");
         char r2= scan.next().charAt(0);
 
         if (r2 == 'y'){
-            System.out.println("insira o nome? ");
+            System.out.println("Qual nome? ");
             String antigoNome = scan.nextLine();
             scan.nextLine();
-            System.out.println("insira o nome que vai alterar: ");
+            System.out.println("insira o novo nome: ");
             String novoNome = scan.nextLine();
-            for (int i = 0; i < nomes.length; i++){
-                if (antigoNome.equalsIgnoreCase(nomes[i])){
+            for (int i = 1; i < nomes.length; i++){
+                if (antigoNome.equals(nomes[i])){
                     nomes[i] = novoNome;
-                    System.out.println(nomes[i]+" "+i);
                 }
+                System.out.println(nomes[i]+" "+i);
             }
         }
-
+        System.out.println("deseja remover algum nome: y/n ");
+        char r3= scan.next().charAt(0);
+        if (r3 == 'y'){
+            System.out.println("informe o nome a ser removido: ");
+            String remover_nome = scan.nextLine();
+            for (int i = 1; i< nomes.length;i++){
+                if (remover_nome == nomes[i]){
+                    nomes[i] = null;
+                }
+                System.out.println(nomes[i]+" "+i);
+            }
+        }
     }
 }
