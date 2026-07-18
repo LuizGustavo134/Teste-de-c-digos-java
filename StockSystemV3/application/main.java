00b6b7038937e2e48550269e3601d706221630df
Dev-Lu <luizgustav796@gmail.com>
2026-07-17 23:16:27 -0300

    comit via teminal idle

--- StockSystemV3/application/main.java
+++ StockSystemV3/application/main.java
@@ -3,5 +3,6 @@ package StockSystemV3.application;
 public class main {
     public static void main(String[]args){
         System.out.println("hello world");
+        System.out.println("Bob esponja");
     }
-} Teste-de-c-digos-java
+}
